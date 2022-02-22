c4544b19af871cf65640d13fddb48c6c10604e69
Lewka-m <87823375+Lewka-m@users.noreply.github.com>
2022-02-22 15:07:00 +0300

    Update Header.tsx

--- src/components/MainPage/NotWorkArea(Side&Head)/Header.tsx
+++ src/components/MainPage/NotWorkArea(Side&Head)/Header.tsx
@@ -15,62 +15,67 @@ import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
 import URL, { CreateCokies, get_cookie, XMLrequest } from "../../Url";
 import useLocalStorage from '../../Hooks/useLocalStorage';
-import {useStyles} from "../../Styles"
+import { useStyles } from "../../Styles"
 import { Typography } from '@mui/material';
 import { DrxContext } from '../../Wrapper';
-import {HiddenNavButton} from './HiddenNav'
+import { HiddenNavButton } from './HiddenNav'
 import { Headerprops } from '../../ComponentInterface';
 
-const DashboardNavbar = (props:Headerprops) => {
+const DashboardNavbar = (props: Headerprops) => {
   const [notifications] = useState([]);
   const [authtoken, setAutnToken] = useLocalStorage(true, "auth")
   let navigate = useNavigate();
   const classes = useStyles();
   const drx = get_cookie("drx");
-  
+
   const HandleSingOut = (event: any) => {
     let params = new Map();
-    params.set('comand','leave');
+    params.set('comand', 'leave');
     XMLrequest(params)
-    CreateCokies("drx","");
+    CreateCokies("drx", "");
   };
- 
+
   return (
-    <AppBar  position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} style={{backgroundColor:"#628eb8"}}>
-      <Toolbar className={classes.customizeToolbar} style = {{minHeight:48}}>
+    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} style={{ backgroundColor: "#628eb8" }}>
+      <Toolbar className={classes.customizeToolbar} style={{ minHeight: 48, height: 40 }}>
         <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            {props.name ? drx.replace(".drx","")+' / '+ props.name: drx.replace(".drx","")}
-          </Typography>
-          
+          variant="h5"
+          noWrap
+          component="div"
+          sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+        >
+          {props.name ? drx.replace(".drx", "") + ' / ' + props.name : drx.replace(".drx", "")}
+        </Typography>
+
         <Box sx={{ flexGrow: 1 }} />
-        
-          <IconButton color="inherit" size="large">
-            <Badge
-              badgeContent={notifications.length}
-              color="primary"
-              variant="dot"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <Link to={'/'} >
+
+        <IconButton color="inherit" size="large">
+          <Badge
+            badgeContent={notifications.length}
+            color="primary"
+            variant="dot"
+          >
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <Link to={'/'} >
           <IconButton color="inherit" size="large" onClick={HandleSingOut}>
             <InputIcon />
-           
+
           </IconButton>
-       </Link>
-        
+        </Link>
+
       </Toolbar>
-      <div id = "HiddenNav" style={!props.open ? {display: "block"}:{display: "none"}}>
-       <HiddenNavButton />
+      <div id="miniMenu" style={!props.open ? { display: "flex", height:"24px" } : { display: "none" }}>
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+        <div id="HiddenNav" style={!props.open ? { display: "block" } : { display: "none" }}>
+          <HiddenNavButton />
+        </div>
       </div>
-      </AppBar>
-    
+    </AppBar>
+
   );
 };
 
