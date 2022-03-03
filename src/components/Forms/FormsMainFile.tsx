2b6c7ba84c0ae3ee272c81997b8a26c1cd3fb627
Stas <stas-roamnov2013@yandex.ru>
2022-03-03 16:05:58 +0300

    Update FormsMainFile.tsx

--- src/components/Forms/FormsMainFile.tsx
+++ src/components/Forms/FormsMainFile.tsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Button, Grid } from "@mui/material";
 import React,{useEffect, useState} from "react";
 import { XMLrequest } from "../Url";
 
@@ -7,15 +7,16 @@ import { XMLrequest } from "../Url";
 
 export default function FormsMainFile(props:any){
     const [dataForms, setDataForms] = useState();
+ 
 
     useEffect(()=>{
         GetSectionForms();
     },[]);
 
     useEffect(()=>{
-        if(dataForms !== undefined){
-            FormDataProcessing(dataForms)
-        }
+       // if(dataForms !== undefined){
+        //     FormDataProcessing(dataForms)
+        // }
     },[dataForms])
 
     const GetSectionForms=()=>{//GET /forms~GetSectionForm?LicGUID=8C5F5163443EBAC78D42B78939951952&SectionID=482 HTTP/1.0
@@ -25,17 +26,110 @@ export default function FormsMainFile(props:any){
         params.set("SectionID", props.id);/////
         json = XMLrequest(params);
         setDataForms(json);
-        console.log(json);
-    } 
+        
+    }
+
+    function BackColor(color:any){
+        switch (color){
+            case "Черный":
+                break;
+            case "Бордовый":
+                break;
+            case "Зеленый":
+                break;
+            case "Коричневый":
+                break;
+            case "Темно-синий":
+                break;
+            case "Пурпурный":
+                break
+            case "Тёмно-зеленый":
+                break
+            case "Серый":
+                break
+            case "Светло-серый":
+                break
+            case "Красный":
+                break
+            case "Светло-зеленый":
+                break
+            case "Желтый":
+                break
+            case "Голубой":
+                break
+            case "Фиолетовый":
+                break
+            case "Светло-голубой":
+                break
+            case "Белый":
+                break
+            case "Подсказка":
+                break
+            case "":
+                break    
+
+            
+        }
+    }
+    
+    
+    function CheckAndReturnComponent(json:any){
+        let ReturnComponent =[],Enabled, Height, Left, Top,Name, Width,  RCDATA, Text
+        Left = json.Left;
+        Top = json.Top;
+        switch(json.Type){
+            case "TImage":
+                RCDATA = json.RCDATA
+                ReturnComponent.push(
+                    <Grid  style={{ position:"absolute" ,left:`${Left}px`, top:`${Top}px`}}>
+                         <img src={`data:image/png;base64,${RCDATA}`} />
+                    </Grid>
+                )
+                break;
+            case "TButton":
+                
+                let BackColorArr, BGC
+                Text = json.Text
+                Height = json.Height
+                Width = json.Width
+                BackColorArr = json["Back-color"]
+                BackColorArr = BackColorArr.split(":")
+
+                ReturnComponent.push(
+                    <Button variant="contained" style={{ position:"absolute", width: `${Width}px`,height:`${Height}px`,left:`${Left}px`, top:`${Top}px`}}>{Text}</Button>
+                )
+                console.log(BackColorArr)
+                break;
+            case "TSectionCheckBox":
+                break;
+            case "TSectionEditor":
+                break;
+        }
+        return ReturnComponent;
+    }
 
     function FormDataProcessing(json:any){
-
+        if(dataForms !== undefined){
+            
+            let val:any, returnAll=[]
+            json = json.Form;
+            for(const [key, value] of Object.entries(json)) {
+                val = value
+                if(val.Type !==undefined){
+                    returnAll.push(
+                      CheckAndReturnComponent(value)  
+                    ) 
+                }
+            } 
+            return returnAll 
+        }
+        
     }
 
 
     return(
-        <Grid id="mainForms">
-
+        <Grid id="mainForms" style={{position:"absolute"}}>
+            {FormDataProcessing(dataForms)}
         </Grid>
     )
 }
