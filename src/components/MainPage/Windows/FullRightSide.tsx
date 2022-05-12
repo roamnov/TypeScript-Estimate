e571b5278f2624cc17b862c277d6ab855b41eb23
Stas <stas-roamnov2013@yandex.ru>
2022-05-12 16:19:18 +0300

    Update FullRightSide.tsx

--- src/components/MainPage/Windows/FullRightSide.tsx
+++ src/components/MainPage/Windows/FullRightSide.tsx
@@ -4,6 +4,7 @@ import Grid from "@mui/material/Grid"
 import {  InfoAboutClick } from "../../ComponentInterface";
 //import init from "../stimweb/tools"
 //import Init from '../stimategrid/test';
+import { Tabs, TabItem } from 'smart-webcomponents-react/tabs';
 import {AppTabs} from './DocTabs/DocTabs';
 import SectionsDBview from '../Sections/dbview';
 import SectionsReportDocuments from '../Sections/ReportDocuments';
@@ -19,57 +20,126 @@ import ModalProgress from '../../Containers/ModalProgress';
 import {tokenProcessingTest} from "../../TokenProcessing"
 import IconButton from '@mui/material/IconButton';
 import axios from 'axios';
+import Scrollbars from 'react-custom-scrollbars-2';
+
 
 export default function FullRightSide(props: InfoAboutClick) {
 
-  const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState(0);
   const [openReportData, setOpenReportData] = React.useState<any>({});
+  const [tabItems, SetTabItems] = React.useState<any>({})
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
+  
+  const [currentHeight, setCurrentHeight] = React.useState(window.innerHeight - 295);
+    
+  const handleResize = () => {
+      setCurrentHeight(window.innerHeight - 295);
+      console.log(window.innerHeight - 189)
+  }
+  React.useEffect(() => {
+      window.addEventListener("resize", handleResize, false);
+      
+  }, []);
+
   let pringReportsDoc:any
-      // pringReportsDoc = document.getElementById('print_reports');
+
   React.useEffect(()=>{
     console.log(openReportData);
+    
     if(!isEmptyObject(openReportData)){
-      pringReportsDoc = document.getElementById(`print_reports${props.id}`);
-      pringReportsDoc.querySelectorAll('.ActivParams').forEach((n: { classList: { remove: (arg0: string) => void; add: (arg0: string) => void; }; }) => {n.classList.remove('ActivParams'); n.classList.add('NoActivParams')})
+      
+      
       let arrOfReportId = openReportData.ViewIdent.split("-");
-      let newReportWindow = document.createElement("div");
       arrOfReportId = arrOfReportId[0].split("Report")
-      newReportWindow.classList.add("Params");
-      newReportWindow.classList.add("ActivParams");
-      newReportWindow.id = "print_reports" + props.id + "_" + arrOfReportId[1];
-      newReportWindow.innerHTML = openReportData.Items[0].content;
-      pringReportsDoc.appendChild(newReportWindow);
+      const id = "print_reports" + props.id + "_" + arrOfReportId[1]
+      console.log(document.getElementById(id));
+      console.log(tabItems)
+      if(document.getElementById(id) === null){
+        let newReportWindow = document.createElement("div");
+        pringReportsDoc = document.getElementById(`print_reports${props.id}`);
+        pringReportsDoc.querySelectorAll('.ActivParams').forEach((n: { classList: { remove: (arg0: string) => void; add: (arg0: string) => void; }; }) => {n.classList.remove('ActivParams'); n.classList.add('NoActivParams')})
+        newReportWindow.classList.add("Params");
+        newReportWindow.classList.add("ActivParams");
+        newReportWindow.id = id;
+        // newReportWindow.innerHTML = openReportData.Items[0].content;
+        pringReportsDoc.appendChild(newReportWindow);
+        SetTabItems({[id]:[{
+          "label":openReportData.Items[0].Title,
+          "pinned":false
+          }]
+        })
+        
+        ReactDOM.render(
+              <Grid item>    
+                  <Tabs scrollMode="paging" id={id+"tabs"} closeButtons tabPosition="bottom" className="Tabs" selectedIndex={0} style={{ height: "calc(100% - 37px)", width: "100%" }} >
+                  <Grid item style={{textTransform:"none",display:"inline-block", height: currentHeight}}>
+                      <TabItem id={id+"item"} style={{textTransform:"none",display:"inline-block", height: currentHeight}} label={openReportData.Items[0].Title} content={openReportData.Items[0].content} >
+                        <Grid item style={{textTransform:"none",display:"inline-block", height: currentHeight}}>
+                          
+                        
+                        </Grid>
+                      </TabItem>
+                      </Grid> 
+                  </Tabs>
+              </Grid>
+              ,newReportWindow);
+              
+      }else{
+        if(!isEmptyObject(tabItems)){
+          for (const [key, value] of Object.entries(tabItems[id])) {
+            console.log(value)
+          }
+          let tabs:any
+          tabs = document.getElementById(id+"tabs");
+          tabs.insert(1, { label: openReportData.Items[0].Title, content: openReportData.Items[0].content , id: "tesstas" });
+          let test = tabs.getTabs();
+          console.log(test)
+        }
+      }
+      
       // ReactDOM.render(<div id='test1'> </div>,pringReportsDoc)
       // pringReportsDoc.innerHTML = openReportData.Items[0].content;
-      if(openReportData.Tools !== undefined){
-        let buttonFromReport:any
-        buttonFromReport = document.getElementById(`buttons_for_section`+props.id);
-        buttonFromReport.querySelectorAll('.ActivParams').forEach((n: { classList: { remove: (arg0: string) => void; add: (arg0: string) => void; }; }) => {n.classList.remove('ActivParams'); n.classList.add('NoActivParams')})
-        let newReportButton = document.createElement("div");
-        let Button = openReportData.Tools.Buttons[0]
-        let report = Button.ViewIdent.split("-");
-        report = report[0].split("Report");
-        let secid = Button.ViewIdent.split("Section");
-        let ID = Button.ID +"-"+ report[1] +"-"+ secid[1]
-        newReportButton.classList.add("ActivParams");//NoActivButtons
-        newReportButton.id = "button_report_token" + props.id + "_" + arrOfReportId[1];
-        buttonFromReport.appendChild(newReportButton);
-        ReactDOM.render(
-          <Grid item>    
-              <Tooltip  title={Button.Hint + Button.ID +"-"+ report[1] +"-"+ secid[1]} arrow>
-                      <IconButton id={ID}  color='primary'  component="span" onClick={reportsHandleToolButton}   >
-                          {ImgURL(Button.Image)}
-                      </IconButton>
-              </Tooltip>
-          </Grid>
-          ,newReportButton)
-      }
+      // if(openReportData.Tools !== undefined){
+      //   let buttonFromReport:any
+      //   buttonFromReport = document.getElementById(`buttons_for_section`+props.id);
+      //   buttonFromReport.querySelectorAll('.ActivParams').forEach((n: { classList: { remove: (arg0: string) => void; add: (arg0: string) => void; }; }) => {n.classList.remove('ActivParams'); n.classList.add('NoActivParams')})
+      //   let newReportButton = document.createElement("div");
+      //   let Button = openReportData.Tools.Buttons[0]
+      //   let report = Button.ViewIdent.split("-");
+      //   report = report[0].split("Report");
+      //   let secid = Button.ViewIdent.split("Section");
+      //   let ID = Button.ID +"-"+ report[1] +"-"+ secid[1]
+      //   newReportButton.classList.add("ActivParams");//NoActivButtons
+      //   newReportButton.id = "button_report_token" + props.id + "_" + arrOfReportId[1];
+      //   buttonFromReport.appendChild(newReportButton);
+      //   ReactDOM.render(
+      //     <Grid item>    
+      //         <Tooltip  title={Button.Hint + Button.ID +"-"+ report[1] +"-"+ secid[1]} arrow>
+      //                 <IconButton id={ID}  color='primary'  component="span" onClick={reportsHandleToolButton}   >
+      //                     {ImgURL(Button.Image)}
+      //                 </IconButton>
+      //         </Tooltip>
+      //     </Grid>
+      //     ,newReportButton)
+      // }
     }
   },[openReportData])
+
+  function ReturnTabs(id:string){
+    if(!isEmptyObject(tabItems)){
+      let returnTabs= [], valueFor:any
+      for (const [key, value] of Object.entries(tabItems[id])) {
+        returnTabs.push(
+          <TabItem id={id+key} style={{textTransform:"none"}} label={<>{valueFor.label}</>}>
+
+          </TabItem>
+        )
+      }
+      return returnTabs
+    }
+  }
 
 
   async function reportsHandleToolButton(event:any){//reports~HandleToolButton?LicGUID=B921C12049AC58E14F039A983633FE8E&ID=117&ReportID=453&SectionID=108&ViewIdent=Report453-Section108&WSM=1 
@@ -181,7 +251,7 @@ export default function FullRightSide(props: InfoAboutClick) {
 
   return (
     
-    <Grid container   direction="column"     justifyContent="center"   sx={{ flexGrow: 1 }}    alignItems="stretch"    >
+    <Grid container   direction="column"     justifyContent="center"   sx={{ flexGrow: 1 }}    alignItems="stretch"   style={{}} >
       <div id ="WorkPlace" style = {{ height: "calc(100% - 48px)"}}>
       <Grid item  >
         {props.isLoading?<div></div>:<Grid  container  direction="row"  justifyContent="flex-start" alignItems="center" >  </Grid>}
